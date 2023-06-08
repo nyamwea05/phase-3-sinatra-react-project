c05dd8d9409153c024ba90963ca357e0ffcf30cd
sandra <nyamweasandra@gmail.com>
2023-06-08 23:11:18 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,27 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  post "/tasks" do
+    pp params
+      task = Task.create(
+        text: params[:text],
+        title: params[:title],
+        category: params[:category],
+        due_date: params[:dueDate],
+        start_time: params[:startTime],
+        end_time: params[:endTime],
+        priority: params[:priority],
+        description: params[:description]
+      )
+      task.to_json
+
+  end
+  get "/task" do
+
+    task=Task.all
+    task.to_json
+  end
+  
+  # ... other routes
 end
+
